1ab1fc77259e4d4b3a9076684e6cca02185f26f8
Rohit Rathod <86867651+rohitrathod9033@users.noreply.github.com>
2025-06-10 23:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function getFormvalue(event) {
-  event.preventDefault();
-  const form = event.target;
-  const firstName = form.fname.value.trim();
-  const lastName = form.lname.value.trim();
-  if (!firstName && !lastName) {
-	alert("Please enter your First and Last Name.");
-	return;
-  }
-  if (!firstName) {
-	alert("Please enter your First Name.");
-	return;
-  }
-  if (!lastName) {
-	alert("Please enter your Last Name.");
-	return;
-  }
-  alert(`${firstName} ${lastName}`);
+function getFormvalue() {
+const form = document.getElementById("form1");
+const firstName = form.fname.value.trim();
+const lastName = form.lname.value.trim();
+
+if (!firstName && !lastName) {
+  alert("Please enter your First and Last Name.");
+  return false;
+}
+if (!firstName) {
+  alert("Please enter your First Name.");
+  return false;
+}
+if (!lastName) {
+  alert("Please enter your Last Name.");
+  return false;
+}
+
+alert(firstName + " " + lastName);
+return false;
 }
